f8548a9d3dbdfff864b7383b775500e86bb987db
ArnavNawal <snawal34@gmail.com>
2020-11-18 19:00:16 +0530

    Angry Birds Scoring

--- sketch.js
+++ sketch.js
@@ -7,11 +7,13 @@ var engine, world;
 var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
+var bg="sprites/bg.png"
+var score=0
 
 var gameState = "onSling";
 
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+    GetTime();
 }
 
 function setup(){
@@ -45,18 +47,25 @@ function setup(){
 }
 
 function draw(){
+    if(backgroundImg)
     background(backgroundImg);
+    
+    textSize(30);
+    fill("white")
+    text("score : "+score,1000,100)
     Engine.update(engine);
     //strokeWeight(4);
     box1.display();
     box2.display();
     ground.display();
     pig1.display();
+    pig1.score();
     log1.display();
 
     box3.display();
     box4.display();
     pig3.display();
+    pig3.score();
     log3.display();
 
     box5.display();
@@ -66,7 +75,8 @@ function draw(){
     bird.display();
     platform.display();
     //log6.display();
-    slingshot.display();    
+    slingshot.display();  
+      
 }
 
 function mouseDragged(){
@@ -85,4 +95,17 @@ function keyPressed(){
     if(keyCode === 32){
        // slingshot.attach(bird.body);
     }
+}
+async function GetTime(){
+    var response = await fetch("http://worldtimeapi.org/api/timezone/Europe/Rome")
+    var responseJSON = await response.json();
+    var hour = responseJSON.datetime.slice(11,13);
+    console.log(hour)
+    if(hour>=06 && hour<=13){
+        bg = "sprites/bg.png"
+    }
+    else{
+        bg = "sprites/bg2.jpg"
+    }
+    backgroundImg=loadImage(bg)
 }
